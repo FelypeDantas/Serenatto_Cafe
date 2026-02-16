def54ebd4b078bd9aaa0bcbc17f2af106641e4df
Felype <felyped03@gmail.com>
2026-02-16 20:36:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 const inputCheck = document.querySelector('#modo-claro');
 const elemento = document.querySelector('body');
 
+window.addEventListener('DOMContentLoaded', () => {
+    const temaSalvo = localStorage.getItem('tema');
+
+    if (temaSalvo) {
+        elemento.setAttribute("data-bs-theme", temaSalvo);
+        inputCheck.checked = temaSalvo === 'dark';
+    } else {
+        const prefereDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        const modoInicial = prefereDark ? 'dark' : 'light';
+        elemento.setAttribute("data-bs-theme", modoInicial);
+        inputCheck.checked = prefereDark;
+    }
+});
+
 inputCheck.addEventListener('change', () => {
     const modo = inputCheck.checked ? 'dark' : 'light';
     elemento.setAttribute("data-bs-theme", modo);
     localStorage.setItem('tema', modo);
 });
-
-window.addEventListener('DOMContentLoaded', () => {
-    const temaSalvo = localStorage.getItem('tema');
-    if (temaSalvo) {
-        elemento.setAttribute("data-bs-theme", temaSalvo);
-        inputCheck.checked = temaSalvo === 'dark';
-    }
-});
-
-const prefereDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-if (prefereDark) {
-  document.body.setAttribute("data-bs-theme", "dark");
-  inputCheck.checked = true;
-}
-
